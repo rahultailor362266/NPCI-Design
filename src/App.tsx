f1543c09d1f0e58739281c4159e6f236492c4229
Rahul Tailor <Rahul.Tailor@drcsystems.local>
2025-10-03 15:28:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import Login from './pages/Login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+const routes = [
+    { path: '/login', element: <Login /> },
+ 
+];
+  return (
+    <>
+     <BrowserRouter>
+        <Routes>
+              {routes.map(({ path, element }) => (
+                <Route
+                key={path}
+                path={path}
+                element={ element}
+                />
+              ))}
+          </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
