1c341284e1e74465175aaf6e05465c0613e7f5cd
Rahul Tailor <Rahul.Tailor@drcsystems.local>
2025-10-08 12:15:29 +0530

    path issue resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-import './App.css'
-import Login from './pages/Login'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css';
+import Login from './pages/Login';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import "swiper/css";
 import "swiper/css/pagination";
 
 function App() {
-const routes = [
+  const routes = [
     { path: '/login', element: <Login /> },
- 
-];
+  ];
+
   return (
-    <>
-     <BrowserRouter>
-        <Routes>
-              {routes.map(({ path, element }) => (
-                <Route
-                key={path}
-                path={path}
-                element={ element}
-                />
-              ))}
-          </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {routes.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+
+        {/* Redirect root "/" to "/login" */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
